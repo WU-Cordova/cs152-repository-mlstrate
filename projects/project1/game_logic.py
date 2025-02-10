3fb168e964af5c7bf0b56f97763390d6590028f2
mlstrate <mlstrate@willamette.edu>
2025-02-10 11:07:29 -0800

    Finished? BagJack

--- projects/project1/game_logic.py
+++ projects/project1/game_logic.py
@@ -14,6 +14,7 @@ class Game:
         self.player_hand = []
         self.dealer_hand = []
         self.game_over = False
+        print("-----------------------")
         print("Welcome to Blackjack!")
         print("")
 
@@ -29,7 +30,7 @@ class Game:
                 else:
                     score += int(card.value)
             
-            if score >= 21 and 'A' in [card.value for card in hand]:
+            if score > 21 and 'A' in [card.value for card in hand]:
                 score -= 10     # implementing the A = 1 or 11
             return score
 
@@ -53,6 +54,16 @@ class Game:
                 # only calculates score based on dealer's first card
             print("")
 
+            if calculate_score(self.dealer_hand) == 21:     # WIN CASE
+                print("")
+                show_hand(self.dealer_hand, "Dealer")
+                print("Dealer has Blackjack! Dealer wins! Better luck next time.")
+                self.game_over = True
+            elif calculate_score(self.player_hand) == 21:   # WIN CASE
+                print("")
+                print("\u265b Player has Blackjack! Player wins!")
+                self.game_over = True
+
 
         def player_choice():
             choice = input("Would you like to (H)it or (S)tay? ")
@@ -66,19 +77,21 @@ class Game:
                 self.player_hand.append(self.deck.draw_random_card())
                 show_hand(self.player_hand, "Player")
                 
-                if calculate_score(self.player_hand) >= 21:
-                    print("Dealer wins! Player busted. Better luck next time!")
+                if calculate_score(self.player_hand) > 21:      # WIN CASE
+                    print("Bust! You went over 21.")
                     print("")
+                    print("Dealer wins! Better luck next time!")
                     self.game_over = True
                 else:
                     print("")
                     player_choice()
 
-            elif choice == 'S':     # stay case
-                pass
+            elif choice == 'S':     # stay case (just for documentation)
+                print("")
 
 
         def dealer_reveal():
+            ''' Dealer reveals hand, draws until reaching 17 '''
             show_hand(self.dealer_hand, "Dealer")
             
             while calculate_score(self.dealer_hand) < 17:
@@ -86,11 +99,50 @@ class Game:
                 self.dealer_hand.append(self.deck.draw_random_card())
                 show_hand(self.dealer_hand, "Dealer")
                 
-                if calculate_score(self.dealer_hand) >= 21:
+                if calculate_score(self.dealer_hand) > 21:      # WIN CASE
+                    print("")
                     print("\u265b Player wins! Dealer busted.")
                     self.game_over = True
-                print("")
+            print("")
+
+
+        def final_score():
+            ''' Final scores are displayed and the one with a score closest to 21 wins. '''
+            print("Final hands:")
+            show_hand(self.player_hand, "Player")
+            show_hand(self.dealer_hand, "Dealer")
+            print("")
+
+            # WIN CASES
+            if calculate_score(self.player_hand) > calculate_score(self.dealer_hand):
+                print("\u265b Player wins!")
+            elif calculate_score(self.dealer_hand) > calculate_score(self.player_hand):
+                print("Dealer wins! Better luck next time.")
+            elif calculate_score(self.dealer_hand) == calculate_score(self.player_hand):
+                print("It's a tie!")
             
+            print("")
+            self.game_over = True
+
+        
+        def replay_choice():
+            print("")
+            replay = input("Would you like to play again? (Y)es or (N)o: ")
+
+            if replay not in ['Y', 'N']:
+                print("Sorry, I don't understand that choice. Your options are Y (Replay) or N (Quit).")
+                print("")
+                replay_choice()
+            elif replay == 'Y':     # replay case
+                print("")
+                blackjack = Game()
+                blackjack.play()
+            elif replay == 'N':     # quit case
+                print("Game over! Thanks for playing!")
+                print("")
+                # will reach end of program
+
+
 
         while not self.game_over:
             init_deal()
@@ -98,8 +150,9 @@ class Game:
                 player_choice()
             if not self.game_over:
                 dealer_reveal()
-            self.game_over = True
-
+            if not self.game_over:
+                final_score()
+        replay_choice()
     
 
 
